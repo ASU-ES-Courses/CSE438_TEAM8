53ad8f6b81e8d3df4dfd5437a7767cc7fcc6a110
Aditi Sonik <asonik@asu.edu>
2017-10-22 02:22:42 -0700

    updated files
    gpio pin enabling completed remaining pwm and dynamic pin selection.

--- Assignment2_Part2/RGBLed.c
+++ Assignment2_Part2/RGBLed.c
@@ -3,10 +3,13 @@
 #include<linux/kernel.h>
 #include<linux/fs.h>
 #include<linux/device.h>
-#include <asm/uaccess.h>
-#include <linux/cdev.h>
+#include<asm/uaccess.h>
+#include<linux/cdev.h>
 #include<linux/slab.h>
-#include <linux/string.h>
+#include<linux/string.h>
+#include <linux/gpio.h>
+#include<linux/delay.h>
+
 
 #define DEVICE_NAME "RGBLed"
 //#define DEVICE_NAME1 "RGBLed_dev1"			// device-1 name to be created and registered
@@ -16,15 +19,26 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Achal Shah & Aditi Sonik");      
 MODULE_DESCRIPTION("LKM of RGB Led");
 
+//Globals
 
-static dev_t dev_num;					//For allocating mojor & minor number
-struct class *RGBLed_class; 				//For allocating class number
-static struct device *RGBLed_device;
+static dev_t dev_num;							//For allocating device number
+struct class *RGBLed_class; 					//For allocating class number
+static struct device *RGBLed_device;			//creating object of DEVICE structure
 
+int R_GPIO, R_LS, R_MUX;
+int G_GPIO, G_LS, G_MUX;
+int B_GPIO, B_LS, B_MUX;
+
+int GPIO_PIN[] = {11,12,13,14,6,0,1,38,40,4,10,5,15,7};
+int LS_PIN[] = {32,28,34,16,36,18,20,-1,-1,22,26,24,42,30};
+int MUX_PIN[] = {-1,45,77,76,-1,66,68,-1,-1,70,74,44,-1,46};
+
+int LedOFF = 0; // To see LED ON or OFF; 0 = OFF
 
 struct RGBLed_dev {
-	struct cdev cdev;				/* The cdev structure */
-	char name[20];		                	/* Name of device*/
+	struct cdev cdev;							// The cdev structure 
+	char name[20];		                		// Name of device
+	char pattern;
 	
 } *RGBLed_dev1;
 
@@ -54,13 +68,144 @@ int RGBLed_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-
-ssize_t RGBLed_write(struct file *file, const char *buff, size_t count, loff_t *ppos)
+ssize_t RGBLed_write(struct file *file, const char *user_buff, size_t count, loff_t *ppos)
 {
+	int status = 0;
 
 	printk(KERN_ALERT"Writing.......through %s function\n", __FUNCTION__);
+	
+	get_user(RGBLed_dev1->pattern, user_buff);
+	switch(RGBLed_dev1->pattern){
+		// case number represented in sequence {'R','G','B','RG','RB','GB','RGB'}
+		case 1:
+			gpio_set_value(R_GPIO, 1);
+			msleep(10);
+			break;
+		case 2:
+			gpio_set_value(R_GPIO, 1);
+			msleep(10);
+			break;
+		case 3:
+			gpio_set_value(B_GPIO, 1);
+			msleep(10);
+			break;
+		case 4:
+			gpio_set_value(R_GPIO, 1);
+			gpio_set_value(G_GPIO, 1);
+			msleep(10);
+			break;
+		case 5:
+			gpio_set_value(R_GPIO, 1);
+			gpio_set_value(B_GPIO, 1);
+			msleep(10);
+			break;
+		case 6:
+			gpio_set_value(G_GPIO, 1);
+			gpio_set_value(B_GPIO, 1);
+			msleep(10);
+			break;
+		case 7:
+			gpio_set_value(R_GPIO, 1);
+			gpio_set_value(G_GPIO, 1);
+			gpio_set_value(B_GPIO, 1);
+			msleep(10);
+			break;
+		default:
+			gpio_set_value(R_GPIO, 0);
+			gpio_set_value(G_GPIO, 0);
+			gpio_set_value(B_GPIO, 0);
+			break;
 
-	return count;									//-1 = failure ; 1 = success
+	}
+	gpio_set_value(R_GPIO, 0);
+	gpio_set_value(G_GPIO, 0);
+	gpio_set_value(B_GPIO, 0);	
+
+	return status;								
+}
+
+//int RGBLed_ioctl(struct inode * inode, struct file * file, unsigned long _r, unsigned int _g, unsigned int _b){
+int RGBLed_ioctl(struct inode * inode, struct file * file, unsigned long x, unsigned int y){
+	int status = 0,_r,_g,_b;
+
+	printk(KERN_ALERT"Configuring.......through %s function\n", __FUNCTION__);
+	_r = 9;
+	_g = 10;
+	_b = 13;
+	//Check valid inputs or not
+
+	//Selecting GPIO Pins
+	R_GPIO = GPIO_PIN[_r];
+	G_GPIO = GPIO_PIN[_g];
+	B_GPIO = GPIO_PIN[_b];
+
+	//Selecting Level Shifter Pins
+	R_LS = LS_PIN[_r];
+	G_LS = LS_PIN[_g];
+	B_LS = LS_PIN[_b];
+
+	//Selecting MUX Pins
+	R_MUX = MUX_PIN[_r];
+	G_MUX = MUX_PIN[_g];
+	B_MUX = MUX_PIN[_b];
+
+	//GPIO PINS------------------//
+	status =  gpio_direction_output(R_GPIO, LedOFF);   		// Set the gpio to be in output mode and turn off
+	status =  gpio_export(R_GPIO, false);					//causes gpio pin to appear in /sys/class/gpio & the second argument prevents the direction from being changed
+	
+	status =  gpio_direction_output(G_GPIO, LedOFF);
+	status =  gpio_export(G_GPIO, false);
+
+	status =  gpio_direction_output(B_GPIO, LedOFF);
+	status =  gpio_export(B_GPIO, false);
+	
+	//LS PINS--------------------//
+	if(R_LS != -1){
+		status =  gpio_direction_output(R_LS, LedOFF);
+		status =  gpio_export(R_LS, false);
+		gpio_set_value(R_LS, 0);
+	}
+
+	if(G_LS != -1){
+		status =  gpio_direction_output(G_LS, LedOFF);
+		status =  gpio_export(G_LS, false);
+		gpio_set_value(G_LS, 0);
+	}
+
+	if(B_LS != -1){
+		status =  gpio_direction_output(B_LS, LedOFF);
+		status =  gpio_export(B_LS, false);
+		gpio_set_value(B_LS, 0);
+	}
+
+	//MUX PINS-----------------------//
+	if(R_MUX != -1){
+		if(R_MUX < 64 || R_MUX > 79){
+			status =  gpio_direction_output(R_MUX, LedOFF);
+		}
+		status =  gpio_export(R_MUX, false);
+		gpio_set_value(R_MUX, 0);
+	}
+
+	if(G_MUX != -1){
+		if(G_MUX < 64 || G_MUX > 79){
+			status =  gpio_direction_output(G_MUX, LedOFF);
+		}
+		status =  gpio_export(G_MUX, false);
+		gpio_set_value(G_MUX, 0);
+	}
+
+	if(B_MUX != -1){
+		if(B_MUX < 64 || B_MUX > 79){
+			status =  gpio_direction_output(B_MUX, LedOFF);
+		}
+		status =  gpio_export(B_MUX, false);
+		gpio_set_value(B_MUX, 0);
+	}
+	//-----------------------------//
+	printk(KERN_ALERT"Configured RGBLed\n", __FUNCTION__);
+	return status;
+
 }
 
 
@@ -69,7 +214,8 @@ struct file_operations RGBLed_fops={
 	.open			= RGBLed_open,         
 	.release		= RGBLed_release,      
 	.write	    	= RGBLed_write,
-	//.unlocked_ioctl = RGBLed_ioctl,                 
+	.unlocked_ioctl = RGBLed_ioctl,  
+	               
 };
 
 
@@ -79,7 +225,7 @@ int __init RGBLed_module_init(void)
 
 	int return1;
 
-	printk(KERN_ALERT"Working on %s function\n", __FUNCTION__);
+	printk(KERN_ALERT"Installing RGBLed device driver by %s function\n", __FUNCTION__);
 	
 	if (alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME)<0){			//Allocating major|minor numbers to char-device of device driver "RGBLed"
 		printk(KERN_ALERT "Device not registered\n");
@@ -116,7 +262,7 @@ int __init RGBLed_module_init(void)
 void __exit RGBLed_module_exit(void)
 {	
 	
-	printk(KERN_ALERT"Working on %s function\n", __FUNCTION__);
+	printk(KERN_ALERT"Uninstalling RGBLed device driver by %s function\n", __FUNCTION__);
 
 	/* Release the major number */
 	unregister_chrdev_region((dev_num), 1);
@@ -136,7 +282,6 @@ void __exit RGBLed_module_exit(void)
 
 module_init(RGBLed_module_init);
 module_exit(RGBLed_module_exit);
-
 
 
 
---
+++ Assignment2_Part2/main.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include<errno.h>
+#include <sys/ioctl.h>
+#include<string.h>
+
+
+int main(){
+
+	int fd = 0,return_value,i;
+	int arr[4] = {-9,-9,-9,-9};						//[PWM,R,G,B] with default "-9" value
+	unsigned long x = 0;
+	int y = 0;
+	int arr_pattern[7] = {1,2,3,4,5,6,7}; //{'R','G','B','RG','RB','GB','RGB'}
+
+	printf("Enter Integer input values in order [ percentage of PWM, R_IO, G_IO, B_IO ]\n");
+	
+	for(i=0;i<4;i++){
+		scanf("%d",&arr[i]);		
+	}
+
+	
+	fd = open("/dev/RGBLed", O_RDWR);						
+	if (fd < 0 ){
+		printf("Can not open RGBLed device file.\n");		
+		//return -1;
+	}
+	else{
+		//return_value = ioctl(fd, arr[1], arr[2], arr[3]);
+		return_value = ioctl(fd,x,y);
+		if(return_value < 0){
+			printf("Unable to write to RGBLed Device or Invalid input\n");
+			errno = 22;//ENIVAL = 22
+		}
+		printf("return from ioctl = %d\n",return_value);
+		
+		while(1){
+			//When PWM is not equal to 0
+			if(arr[0] != 0){
+				for(i=0;i<7;i++){
+					write(fd,&arr_pattern[i],sizeof(arr_pattern[i]));
+				}
+			}
+			// When PWM is 0
+			else 
+				write(fd,"0",sizeof(int));
+		}
+		
+	}
+
+
+
+	close(fd);
+
+	return 0;
+}
