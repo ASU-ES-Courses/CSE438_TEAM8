e3652687c840ea86cdcfeaf16a57d67b70c8ed31
Aditi Sonik <asonik@asu.edu>
2017-11-06 01:46:27 -0700

    Led display
    working LED display.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<sys/ioctl.h>
+#include<pthread.h>
 #include<linux/spi/spidev.h>
 
 #include"Gpio_func.h"
@@ -21,11 +22,11 @@ int SCK_GPIO = 7, SCK_LS = 30, SCK_PULLUP = 31, SCK_MUX = 46;
 	IO12 -> MISO/CS 	| GPIO 15	42(L) 	43(L)		-
 	IO13 -> SCK 		| GPIO 07	30(L) 	31(L)		46(H)
 	*/
+pthread_t  thread_id[2];
 
 void IOsetup(void){
 
 	int FdExport, SetDir, SetVal;
-
 	
 	//---------------GPIO PINS---------------------//
 	FdExport = gpio_export(MOSI_GPIO);
@@ -35,6 +36,10 @@ void IOsetup(void){
 	SetDir = gpio_set_dir(MOSI_GPIO, 1);
 	if (SetDir < 0){
 		printf("\n MOSI_GPIO direction set failed\n");
+	}
+	SetVal = gpio_set_value(MOSI_GPIO, 1);
+	if (SetVal < 0){
+		printf("\n MOSI_GPIO Value set failed\n");
 	}
 
 	FdExport = gpio_export(CS_GPIO);
@@ -58,10 +63,12 @@ void IOsetup(void){
 	if (SetDir < 0){
 		printf("\n SCK_GPIO direction set failed\n");
 	}
-	
+	SetVal = gpio_set_value(SCK_GPIO, 1);
+	if (SetVal < 0){
+		printf("\n SCK_GPIO Value set failed\n");
+	}
 
 	//---------------LS PINS-----------------------//
-
 	FdExport = gpio_export(MOSI_LS);
 	if(FdExport < 0){
 		printf("MOSI_LS export error\n");		
@@ -225,34 +232,76 @@ void IOclose(void){
 	}
 }
 
-int main(){
+void *Display(){
 
 	int fdLed = 0,return_value = 0;
-	int fdMOSI, fdCS, fdSCK, len,i;
+	int fdCS, len,i;
 	char buf[64];
 
-	/*unsigned char RunLeft[8] = {};
-	unsigned char WalkLeft[8] = {};
-	unsigned char RunRight[8] = {};
-	unsigned char WalkLeft[8] = {};*/
-
 	unsigned char sentMsg[2] = {0,0}, receiveMsg[2] = {0,0};
-	unsigned char dataInput[] = {
-								//0x0C,0x00, //shutdown								
-								0x09,0x00,
-								0x0A,0x0F,
-								//0x0B,0x07,
-								0x01,0x00,
-								0x02,0x00,
-								0x03,0x00,
-								0x04,0x00,
-								0x05,0x00,
-								0x06,0x00,
-								0x07,0x00,
-								0x08,0x00,
-								//0xFF,0x00	//to off all leds
-								//0xFF,0x01	//to on all leds
-								//0x0C,0x00	//shutdown
+
+	unsigned char StillRight[] = {
+								0x0F,0x00,	//Display test
+								0x0C,0x01,	//Shutdown					
+								0x09,0x00,	//Decode
+								0x0A,0x02,	//Intensity
+								0x0B,0x07,	//Scan Limit
+								0x01,0x06,
+								0x02,0x06,
+								0x03,0x06,
+								0x04,0x83,
+								0x05,0x7F,
+								0x06,0x24,
+								0x07,0x22,
+								0x08,0x63
+							};
+
+	unsigned char StillLeft[] = {
+								0x0F,0x00,	//Display test
+								0x0C,0x01,	//Shutdown					
+								0x09,0x00,	//Decode
+								0x0A,0x02,	//Intensity
+								0x0B,0x07,	//Scan Limit
+								0x01,0x60,
+								0x02,0x60,
+								0x03,0x60,
+								0x04,0xC1,
+								0x05,0xFE,
+								0x06,0x24,
+								0x07,0x44,
+								0x08,0xC6
+							};
+
+	unsigned char RunRight[] = {
+								0x0F,0x00,	//Display test
+								0x0C,0x01,	//Shutdown					
+								0x09,0x00,	//Decode
+								0x0A,0x02,	//Intensity
+								0x0B,0x07,	//Scan Limit
+								0x01,0x06,
+								0x02,0x06,
+								0x03,0x06,
+								0x04,0x03,
+								0x05,0x7F,
+								0x06,0xA2,
+								0x07,0x32,
+								0x08,0x16
+							};
+
+	unsigned char RunLeft[] = {
+								0x0F,0x00,	//Display test
+								0x0C,0x01,	//Shutdown					
+								0x09,0x00,	//Decode
+								0x0A,0x02,	//Intensity
+								0x0B,0x07,	//Scan Limit
+								0x01,0x60,
+								0x02,0x60,	
+								0x03,0x60,
+								0x04,0xC0,
+								0x05,0xFE,
+								0x06,0x45,
+								0x07,0x4C,
+								0x08,0x68
 							};
 
 	struct spi_ioc_transfer  message_structure = {
@@ -260,7 +309,7 @@ int main(){
 		.rx_buf = (unsigned long)receiveMsg,
 		.len = sizeof(sentMsg), //array size
 		.cs_change = 1,
-		.speed_hz = 500000,
+		.speed_hz = 10000000,
 		.bits_per_word = 8,
 	};
 
@@ -269,44 +318,107 @@ int main(){
 		printf("error in opening spi device\n");
 	}
 
-	IOsetup();
-
-	len = snprintf(buf, sizeof(buf), "/sys/class/gpio/gpio%d/value", MOSI_GPIO);
-	if(len<0){
-		printf("snprintf error\n");
-	}	 
-	fdMOSI = open(buf, O_WRONLY);
-
 	len = snprintf(buf, sizeof(buf), "/sys/class/gpio/gpio%d/value", CS_GPIO);
 	if(len<0){
 		printf("snprintf error\n");
 	}		
 	fdCS = open(buf, O_WRONLY);
 
-	len = snprintf(buf, sizeof(buf), "/sys/class/gpio/gpio%d/value", SCK_GPIO);
-	if(len<0){
-		printf("snprintf error\n");
-	}
-	fdSCK = open(buf, O_WRONLY);
-
-	/*write(fdMOSI, "1", 1);
-	
-	write(fdSCK, "1", 1);*/
-
-	for(i=0;i<20;i++){
+	for(i=0;i<26;i = i+2){
 		
-		sentMsg[0] = dataInput[i]; //MSB
-		sentMsg[1] = dataInput[i+1]; //LSB
+		sentMsg[0] = StillRight[i]; 					//MSB
+		sentMsg[1] = StillRight[i+1]; 					//LSB
 		return_value = ioctl(fdLed,SPI_IOC_MESSAGE(1),&message_structure);
 		if(return_value < 0){
 			printf("error in ioctl\n");
 		}	
-		write(fdCS, "1", 1);
-		write(fdCS, "0", 1);
+		write(fdCS, "1", 1);							//setting clock high will push data from shift register
+		write(fdCS, "0", 1);							//setting clock low will make shift register ready to receive input
 	}
 	sleep(5);
-	printf("terminating\n");
-	IOclose();
+
+	for(i=0;i<26;i = i+2){
+		
+		sentMsg[0] = RunRight[i]; 						//MSB
+		sentMsg[1] = RunRight[i+1]; 					//LSB
+		return_value = ioctl(fdLed,SPI_IOC_MESSAGE(1),&message_structure);
+		if(return_value < 0){
+			printf("error in ioctl\n");
+		}	
+		write(fdCS, "1", 1);							//setting clock high will push data from shift register
+		write(fdCS, "0", 1);							//setting clock low will make shift register ready to receive input
+	}
+	sleep(5);
+
+	for(i=0;i<26;i = i+2){
+		
+		sentMsg[0] = StillLeft[i]; 						//MSB
+		sentMsg[1] = StillLeft[i+1]; 					//LSB
+		return_value = ioctl(fdLed,SPI_IOC_MESSAGE(1),&message_structure);
+		if(return_value < 0){
+			printf("error in ioctl\n");
+		}	
+		write(fdCS, "1", 1);							//setting clock high will push data from shift register
+		write(fdCS, "0", 1);							//setting clock low will make shift register ready to receive input
+	}
+	sleep(5);
+
+	for(i=0;i<26;i = i+2){
+		
+		sentMsg[0] = RunLeft[i]; 						//MSB
+		sentMsg[1] = RunLeft[i+1]; 						//LSB
+		return_value = ioctl(fdLed,SPI_IOC_MESSAGE(1),&message_structure);
+		if(return_value < 0){
+			printf("error in ioctl\n");
+		}	
+		write(fdCS, "1", 1);							//setting clock high will push data from shift register
+		write(fdCS, "0", 1);							//setting clock low will make shift register ready to receive input
+	}
+	sleep(5);
+
 	close(fdLed);
+
+return NULL;
+}
+
+int main(){
+
+	int i;
+	int thread_return[2];
+	//setting up IO pins
+	IOsetup();
+
+	//Thread for LED Display
+	thread_return[0] = pthread_create( &thread_id[0], NULL, &Display, NULL);
+	if(thread_return[0] != 0){
+		printf("Display thread create error");
+	}
+	else
+	printf("Display thread created\n");
+
+/*	//Thread for Distance sensor
+	thread_return[1] = pthread_create( &thread_id[1], NULL, &DistanceSensor, NULL);
+	if(thread_return[1] != 0){
+		printf("Distance Sensor thread create error");
+	}
+	else
+	printf("Distance Sensor thread created\n");
+
+	for(i=0;i<2;i++){
+		thread_return[i] = pthread_join(thread_id[i], NULL);
+		if(thread_return[i] != 0){
+			printf("thread join error\n");
+		}
+	}
+*/
+	thread_return[0] = pthread_join(thread_id[0], NULL);
+	if(thread_return[0] != 0){
+		printf("thread join error\n");
+	}	
+
+	printf("terminating\n");
+	//resetting IO pins
+	IOclose();
+	
 	return 0;
 }
